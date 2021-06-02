7fddc47298b3830594fa6ca57d306ae6ca54ebb7
jkutkut <jorgeregonzalez@gmail.com>
2021-06-02 12:27:55 +0200

    Point is now an ES6 class

--- point.js
+++ point.js
@@ -1,26 +1,46 @@
-function Point(x, y, index){
-  this.x = x;
-  this.y = y;
-  this.index = index;//to search in matrix
-  
-  //A*
-  this.h;
-  this.g;
-  this.f;
-  this.previous = undefined;
-  
-  this.show = function(color){
-    stroke(color);
-    strokeWeight(10);
-    point(this.x, this.y);
-  }
-  this.getNeighbors = function(){
-    let neighbors = [];
-    for(let i = index, j = 0; j < nodes; j++){
-      if(mapa[i][j] == 1){//if conected
-        neighbors.push(coordP[j]);//add it
-      }
+class Point {
+    constructor (x, y, index) {
+        this._x = x;
+        this._y = y;
+        this.index = index;
+
+        // A* variables
+        this.h;
+        this.g;
+        this.f;
+        this.previous = undefined;
     }
-    return neighbors;
-  }
+
+    // Getters and setters
+    get x() {
+        return this._x;
+    }
+
+    get y() {
+        return this._y;
+    }
+
+    get index() {
+        return this._index;
+    }
+
+    set index(newIndex) {
+        this._index = newIndex;
+    }
+
+    show(color) {
+        stroke(color);
+        strokeWeight(10);
+        point(this.x, this.y);
+    }
+
+    getNeighbors() {
+        let neighbors = [];
+        for(let i = this.index, j = 0; j < nodes; j++){
+            if(mapa[i][j] == 1){ // if connected
+                neighbors.push(coordP[j]); // add it
+            }
+        }
+        return neighbors;
+    }
 }
